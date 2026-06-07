6c664f2dd627cff8abd895f8efd7c28777e227c6
larabinotti <lara.binotti@sptech.school>
2026-06-07 20:53:25 -0300

    Revert "feat: implementa autenticação de empresas com tratamento de CNPJ"
    This reverts commit 8224420eef6bcd24c7eab85eb58f74d1041d852a.
    
    feat: adiciona todos passos de cadastro de empresa

--- src/routes/empresas.js
+++ src/routes/empresas.js
@@ -3,16 +3,13 @@ var router = express.Router();
 
 var empresaController = require("../controllers/empresaController");
 
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/cadastrar", function (req, res) {
-  empresaController.cadastrar(req, res);
+    empresaController.cadastrar(req, res);
 })
 
-router.post("/autenticar", function (req, res) {
-  empresaController.autenticar(req, res);
-});
-
 router.get("/buscar", function (req, res) {
-  empresaController.buscarPorCnpj(req, res);
+    empresaController.buscarPorCnpj(req, res);
 });
 
 router.get("/buscar/:id", function (req, res) {
